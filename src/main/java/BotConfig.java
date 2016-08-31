90d00e60863287391d4e525976f74f211a592c04
Christopher Goh <chrisgzf@gmail.com>
2016-09-01 03:54:30 +0800

    Changed Telegram API token handling

---
+++ src/main/java/BotConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Created by chris on 1/9/16.
+ */
+
+public class BotConfig {
+
+    // really, just replace the contents with this variable with your bot username and token
+    // i'm just doing it this way so i don't have to commit my token to github
+    public static final String BOT_USERNAME = BotCredentials.BOT_USERNAME;
+    public static final String BOT_TOKEN = BotCredentials.BOT_TOKEN;
+}
